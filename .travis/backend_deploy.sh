8f6fc6a0ed6b8f6557013b907de48d19dcc7977f
kryvokhyzha <kriwohizha@gmail.com>
2019-11-06 02:24:37 +0200

    Edited Travis config file

--- .travis/backend_deploy.sh
+++ .travis/backend_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd backend/ || exit
+echo Starting deploy...
 npm run deploy
 cd ..
--- .travis/backend_tests_and_lint.sh
+++ .travis/backend_tests_and_lint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd backend/ || exit
+echo Starting tests for backend...
 npm run lint
 npm run test
 cd ..
