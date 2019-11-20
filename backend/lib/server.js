9e6c2245f9fe4d11141346a4c6ef285a76ed7090
Dmitrylyk <dilykmail@gmail.com>
2019-11-20 19:52:47 +0200

    Minor fixes and added base for p2p connection

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// const fs = require('fs');
+const fs = require('fs');
 
 const mongoose = require('mongoose');
 const { dbhost, dbpassword, dbuser } = require('./config/db.js');
 
 const fastify = require('fastify')({
-    // https: {
-    //     key: fs.readFileSync('./config/security/key.pem'),
-    //     cert: fs.readFileSync('./config/security/cert.pem')
-    // },
+    https: {
+        key: fs.readFileSync('./config/security/key.pem'),
+        cert: fs.readFileSync('./config/security/cert.pem')
+    },
     logger: true
 });
 const swagger = require('./config/swagger.js');
